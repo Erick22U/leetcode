50d624d0ae3e76210a8774ec146ef91982b74a34
Erick22U <78757758+Erick22U@users.noreply.github.com>
2022-06-04 18:42:02 -0500

    Time: 0 ms (100.00%), Space: 53.4 MB (70.79%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -4,20 +4,17 @@ class Solution {
         int l = 0;
         int r = nums.length - 1;
         
-        while (r >= l) {
-            int mid = (l + r) / 2;
+        while(l <= r){
+            int mid = l + (r - l)/2;
             
-            if(nums[mid] == target){
-                return mid;
-            }
-            
-            if(nums[mid] > target){
+            if(nums[mid] > target)
                 r = mid - 1;
-            }
-            else{
+            else if(nums[mid] < target)
                 l = mid + 1;
-            }
+            else
+                return mid;
         }
+        
         return -1;
     }
 }
